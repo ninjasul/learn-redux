e8186fd58271678524a663b18c043bccd313efbd
ninjasul@gmail.com <ninjasul@gmail.com>
2020-01-07 17:22:25 +0900

    TodoList 개발 중

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoList({ todos, onToggle }) {
+    return (
+        <ul>
+            <li todos.map
+        </ul>
+    );
+    return <div />;
+}
+
+export default TodoList;
---
+++ src/components/Todos.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import TodoList from "./TodoList";
+
+function Todos({todos, onCreate, onToggle}) {
+    const [text, setText] = useState('');
+    const onChange = (e) => {
+        setText(e.target.value);
+    };
+    const onSubmit = (e) => {
+        // 새로고침 방지
+        e.preventDefault();
+        onCreate(text);
+        setText('');
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="할 일을 입력하세요..."
+                    value={text}
+                    onChange={onChange}
+                />
+                <button type="submit">등록</button>
+            </form>
+            <TodoList
+                todos={todos}
+                onToggle={onToggle}
+            />
+        </div>
+    );
+}
+
+export default Todos;
