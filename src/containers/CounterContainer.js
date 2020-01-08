dd60ed9d403aec3500b8240fc76eeba123c6108c
ninjasul@gmail.com <ninjasul@gmail.com>
2020-01-08 11:20:19 +0900

    주석 추가

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -4,6 +4,7 @@ import {decrease, increase, setDiff} from "../modules/counter";
 import Counter from "../components/Counter";
 
 function CounterContainer() {
+    // useSelector 사용 시 여러 개의 값을 가지고 새로운 객체를 생성하는 경우 성능 문제가 발생할 수 있음.
     // shallowEqual 함수는 객체 내의 1 depth 필드들이 일치하는 지 여부를 체크 해 줌.
     // 일치하는 경우 리렌더링 수행하지 않음.
     const { number, diff } = useSelector(
